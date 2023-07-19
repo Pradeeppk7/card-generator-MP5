d2064c6b0e1b5fdf6b0036da47c4dab0d1cc2635
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2023-07-20 01:13:06 +0530

    Validation completed

--- src/App.js
+++ src/App.js
@@ -16,63 +16,76 @@ function App() {
 
   function dataHandle(e) {
     e.preventDefault();
+    if (isValid.name && isValid.nu && isValid.month && isValid.year) {  
+      setName(initialState.n);
+      setCardNumber(initialState.cn);
+      setExpMonth(initialState.em);
+      setExpYear(initialState.ey);
+      setCvc(initialState.cv);
+      
+    }
+    else {
 
-    setName(initialState.n);
-    setCardNumber(initialState.cn);
-    setExpMonth(initialState.em);
-    setExpYear(initialState.ey);
-    setCvc(initialState.cv);
+      alert("Invalid data");
+    }
   }
 
-  function validation() {
+  function validation1() {
     if (!initialState.n) {
       setErrors({ ...formErrors, name: 'Please fill this field' });
       isValid.name = false;
-    } else {
+    } else if (initialState.n) {
       isValid.name = true;
       setErrors({ ...formErrors, name: '' });
     }
-    if (!initialState.cn) {
-      setErrors({ ...formErrors, nu: 'Please fill this field' });
+  }
+  function validation2() {
+    if (!initialState.cn || !(initialState.cn.length === 16) || !/^\d+$/.test(initialState.cn)) {
+      setErrors({ ...formErrors, nu: 'Please fill the valid numbers' });
       isValid.nu = false;
     } else {
       isValid.nu = true;
       setErrors({ ...formErrors, nu: '' });
     }
-    //  if (!initialState.em) {
-    //   setErrors({ ...formErrors, month: "Please fill this field" });
-    //   isValid.month = false;
-    // }
-    // else {
-    //   isValid.month = true;
-    //   setErrors({ ...formErrors, month: "" });
-    // }
-    //  if (!initialState.ey) {
-    //   setErrors({ ...formErrors, year: "Please fill this field" });
-    //   isValid.year = false;
-    // }
-    // else {
-    //   isValid.year = true;
-    //   setErrors({ ...formErrors, year: "" });
-    // }
-    //  if (!initialState.cv) {
-    //   setErrors({ ...formErrors, cvc: "Please fill this field" });
-    //   isValid.year = false;
-    // }
-    // else {
-    //   isValid.year = true;
-    //   setErrors({ ...formErrors, cvc: "" });
-    // }
-
-    // console.log(isValid.name,formErrors.name)
-    // //return  formErrors;
   }
+  function validation3() {
+    if (!initialState.em  || !/^\d+$/.test(initialState.em) || !(initialState.em.length === 2)|| !(initialState.em<=12) ) {
+      setErrors({ ...formErrors, expdate: 'Please fill the valid Expire date' });
+      isValid.month = false;
+    } else {
+      isValid.month = true;
+      setErrors({ ...formErrors, expdate: '' });
+    }
+    
+  }
+  function validation4() {
+    if (!initialState.ey || !(/^\d+$/.test(initialState.ey)) ||!(initialState.ey.length === 2)) {
+      setErrors({ ...formErrors, expdate: 'Please fill the valid Expire date' });
+      isValid.year = false;
+    } else {
+      isValid.year = true;
+      setErrors({ ...formErrors, expdate: '' });
+    }
+  }
+  function validation5() {
+    if (!initialState.cv || !(/^\d+$/.test(initialState.ey)) || !(initialState.cv.length === 3)) {
+      setErrors({ ...formErrors, cvc: 'Please fill this field' });
+      isValid.year = false;
+    } else {
+      isValid.year = true;
+      setErrors({ ...formErrors, cvc: '' });
+    }
+  }
+
+  // console.log(isValid.name,formErrors.name)
+  // //return  formErrors;
+
   return (
     <main>
       <div className="home">
         <div className="leftsection"></div>
         <div className="rightsection">
-          <form>
+          <form id='forms'>
             <label className="labelin">CARDHOLDER NAME</label>
             <br />
             <br />
@@ -83,7 +96,7 @@ function App() {
               onChange={(e) =>
                 setInitial({ ...initialState, n: e.target.value })
               }
-              onBlur={validation}
+              onBlur={validation1}
             />
             <p className="redalert"> {formErrors.name}</p>
 
@@ -92,13 +105,16 @@ function App() {
             <br />
             <br />
             <input
+              maxLength={16}
               className="colinput"
               type="text"
               placeholder="e.g. 1234 5678 9123 0000"
               onChange={(e) =>
                 setInitial({ ...initialState, cn: e.target.value })
               }
+              onBlur={validation2}
             />
+            <p className="redalert"> {formErrors.nu}</p>
 
             <br />
             <label className="labelin" id="explabel">
@@ -115,31 +131,45 @@ function App() {
                 className="rowinput"
                 type="text"
                 placeholder="MM"
+                maxLength={2}
+                minLength={2}
                 onChange={(e) =>
                   setInitial({ ...initialState, em: e.target.value })
                 }
+                onBlur={validation3}
               />
               <input
                 id="exyearinput"
                 className="rowinput"
                 type="text"
                 placeholder="YY"
+                maxLength={2}
+                minLength={2}
                 onChange={(e) =>
                   setInitial({ ...initialState, ey: e.target.value })
                 }
+                onBlur={validation4}
               />
               <input
                 id="cvcinput"
                 className="rowinput"
                 type="text"
+                maxLength={3}
+                minLength={3}
                 placeholder="e.g. 123"
                 onChange={(e) =>
                   setInitial({ ...initialState, cv: e.target.value })
                 }
+                onBlur={validation5}
               />
+            </div>
+            <div className="rowin">
+              <p className="redalert"> {formErrors.expdate}</p>
+              <p className="redalert" id='cvcalert'> {formErrors.cvc}</p>
             </div>
             <button onClick={dataHandle}>Confirm</button>
           </form>
+          
         </div>
         <div className="cardfront">
           <Cardfront
